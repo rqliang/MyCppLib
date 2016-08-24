f5878a34eaf593fe9d89a48df4a5762fbf9eb95b
Ruqiang Liang <ruqiang.liang@gmail.com>
2016-08-24 12:01:38 -0700

    Create CorrectLigand.cpp

---
+++ CorrectLigand.cpp
@@ -0,0 +1,63 @@
+//
+//  main.cpp
+//  CorrectLigand
+//
+//  Created by Ruqiang Liang on 8/12/16.
+//  Copyright © 2016 Ruqiang Liang. All rights reserved.
+//
+
+#include <iostream>
+#include <fstream>
+#include <map>
+using namespace std;
+
+static const string HETATM = "HETATM";
+
+map<string, string> makeDict(string fn) {
+    map<string, string> atomPairs;
+    ifstream ligand(fn);
+    string str, key, value;
+    size_t pos;
+    while (getline(ligand, str)) {
+        if (str[0] == '#') continue;
+        pos = str.find('\t');
+        key = str.substr(0,pos);
+        value = str.substr(pos+1,str.size());
+        atomPairs.insert(pair<string, string>(key, value));
+    }
+    return atomPairs;
+}
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    if (argc < 3){
+        cerr << "Usage: " << argv[0] << " ligand.def input.pdb output.pdb" << endl;
+        return -1;
+    }
+    string ligand = argv[1];
+    string inPDB = argv[2];
+    string outPDB = argv[3];
+    string line;
+    //string nline;
+    size_t pos;
+    ofstream myWrite(outPDB);
+    ifstream inputFile(inPDB);
+    map<string, string> atomPairs = makeDict(ligand);
+    while (getline(inputFile, line)) {
+        if (line.find(HETATM)==0) {
+            map<string, string>::iterator it;
+            for (it = atomPairs.begin(); it != atomPairs.end(); ++it) {
+                if ((pos = line.find(it->first)) != string::npos) {
+                    line.replace(pos, (it -> first).size(), it->second);
+                    //myWrite << line << endl;
+                    break;
+                }
+            }
+            myWrite << line << endl;
+        } else {
+            myWrite << line << endl;
+        }
+    }
+
+    return 0;
+}
