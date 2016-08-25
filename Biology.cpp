b1b10e4b1884e9880169e927fdede7d065ffd596
Ruqiang Liang <ruqiang.liang@gmail.com>
2016-08-24 22:14:52 -0700

    Add files via upload

--- Biology.cpp
+++ Biology.cpp
@@ -19,6 +19,204 @@ NT complement(const NT& x) {
     }
 }
 
+AA3 AA123(AA1 x) {
+    switch (x) {
+        case AA1::A:
+            return AA3::Ala;
+        case AA1::C:
+            return AA3::Cys;
+        case AA1::D:
+            return AA3::Asp;
+        case AA1::E:
+            return AA3::Glu;
+        case AA1::F:
+            return AA3::Phe;
+        case AA1::G:
+            return AA3::Gly;
+        case AA1::H:
+            return AA3::His;
+        case AA1::I:
+            return AA3::Ile;
+        case AA1::K:
+            return AA3::Lys;
+        case AA1::L:
+            return AA3::Leu;
+        case AA1::M:
+            return AA3::Met;
+        case AA1::N:
+            return AA3::Asn;
+        case AA1::P:
+            return AA3::Pro;
+        case AA1::Q:
+            return AA3::Gln;
+        case AA1::R:
+            return AA3::Arg;
+        case AA1::S:
+            return AA3::Ser;
+        case AA1::T:
+            return AA3::Thr;
+        case AA1::V:
+            return AA3::Val;
+        case AA1::W:
+            return AA3::Trp;
+        case AA1::Y:
+            return AA3::Tyr;
+        case AA1::X:
+            return AA3::Any;
+    }
+}
+
+AA1 AA321(AA3 x) {
+    switch (x) {
+        case AA3::Ala :
+            return AA1::A;
+        case AA3::Cys :
+            return AA1::C;
+        case AA3::Asp :
+            return AA1::D;
+        case AA3::Glu :
+            return AA1::E;
+        case AA3::Phe :
+            return AA1::F;
+        case AA3::Gly :
+            return AA1::G;
+        case AA3::His :
+            return AA1::H;
+        case AA3::Ile :
+            return AA1::I;
+        case AA3::Leu :
+            return AA1::L;
+        case AA3::Lys :
+            return AA1::K;
+        case AA3::Met :
+            return AA1::M;
+        case AA3::Asn :
+            return AA1::N;
+        case AA3::Pro :
+            return AA1::P;
+        case AA3::Gln :
+            return AA1::Q;
+        case AA3::Arg :
+            return AA1::R;
+        case AA3::Ser :
+            return AA1::S;
+        case AA3::Thr :
+            return AA1::T;
+        case AA3::Val :
+            return AA1::V;
+        case AA3::Trp :
+            return AA1::W;
+        case AA3::Any :
+            return AA1::X;
+        case AA3::Tyr :
+            return AA1::Y;
+    }
+}
+
+char AA1toChar(AA1 x) {
+    switch (x) {
+        case AA1::A:
+            return 'A';
+        case AA1::C:
+            return 'C';
+        case AA1::D:
+            return  'D';
+        case AA1::E:
+            return 'E';
+        case AA1::F:
+            return 'F';
+        case AA1::G:
+            return 'G';
+        case AA1::H:
+            return 'H';
+        case AA1::I:
+            return 'I';
+        case AA1::K:
+            return 'K';
+        case AA1::L:
+            return 'L';
+        case AA1::M:
+            return 'M';
+        case AA1::N:
+            return 'N';
+        case AA1::P:
+            return 'P';
+        case AA1::Q:
+            return 'Q';
+        case AA1::R:
+            return 'R';
+        case AA1::S:
+            return 'S';
+        case AA1::T:
+            return 'T';
+        case AA1::V:
+            return 'V';
+        case AA1::W:
+            return 'W';
+        case AA1::Y:
+            return 'Y';
+        case AA1::X:
+            return 'X';
+    }
+}
+
+char AA3toChar(AA3 x) {
+    AA1 y = AA321(x);
+    return AA1toChar(y);
+}
+
+std::string AA3toStr(AA3 x){
+    switch (x) {
+        case AA3::Ala :
+            return "Ala";
+        case AA3::Cys :
+            return "Cys";
+        case AA3::Asp :
+            return "Asp";
+        case AA3::Glu :
+            return "Glu";
+        case AA3::Phe :
+            return "Phe";
+        case AA3::Gly :
+            return "Gly";
+        case AA3::His :
+            return "His";
+        case AA3::Ile :
+            return "Ile";
+        case AA3::Leu :
+            return "Leu";
+        case AA3::Lys :
+            return "Lys";
+        case AA3::Met :
+            return "Met";
+        case AA3::Asn :
+            return "Asn";
+        case AA3::Pro :
+            return "Pro";
+        case AA3::Gln :
+            return "Gln";
+        case AA3::Arg :
+            return "Arg";
+        case AA3::Ser :
+            return "Ser";
+        case AA3::Thr :
+            return "Thr";
+        case AA3::Val :
+            return "Val";
+        case AA3::Trp :
+            return "Trp";
+        case AA3::Any :
+            return "Any";
+        case AA3::Tyr :
+            return "Tyr";
+    }
+}
+
+std::string AA1toStr(AA1 x) {
+    AA3 y = AA123(x);
+    return AA3toStr(y);
+}
+
 float ntmw(const NT& x){
     switch (x) {
         case NT::G: return 347.2;
--- Biology.h
+++ Biology.h
@@ -14,9 +14,145 @@
 #include <iostream>
 
 enum class NT{G,A,T,C,N,M,K};
+enum class AA1{A,C,D,E,F,G,H,I,K,L,M,N,P,Q,R,S,T,V,W,Y,X};
+enum class AA3{Ala,Cys,Asp,Glu,Phe,Gly,His,Ile,Lys,Leu,Met,Asn,Pro,Gln,Arg,Ser,Thr,Val,Trp,Tyr,Any};
+AA3 AA123(AA1 x);
+AA1 AA321(AA3 x);
+char AA1toChar(AA1 x);
+char AA3toChar(AA3 x);
+std::string AA3toStr(AA3 x);
+std::string AA1toStr(AA1 x);
 
 NT complement(const NT& x);
 float ntmw(const NT& x);
+
+class Protein{
+    std::vector<AA1> prot;
+public:
+    Protein(std::initializer_list<AA1> ilAA1): prot{ilAA1} {}
+    Protein(std::vector<AA1> x): prot{x}{}
+    Protein(std::string str) {
+        for (const char& x: str) {
+            switch (x) {
+                case 'A':
+                case 'a':
+                    prot.push_back(AA1::A);
+                    break;
+                case 'C':
+                case 'c':
+                    prot.push_back(AA1::C);
+                    break;
+                case 'D':
+                case 'd':
+                    prot.push_back(AA1::D);
+                    break;
+                case 'E':
+                case 'e':
+                    prot.push_back(AA1::E);
+                    break;
+                case 'F':
+                case 'f':
+                    prot.push_back(AA1::F);
+                    break;
+                case 'G':
+                case 'g':
+                    prot.push_back(AA1::G);
+                    break;
+                case 'H':
+                case 'h':
+                    prot.push_back(AA1::H);
+                    break;
+                case 'I':
+                case 'i':
+                    prot.push_back(AA1::I);
+                    break;
+                case 'K':
+                case 'k':
+                    prot.push_back(AA1::K);
+                    break;
+                case 'L':
+                case 'l':
+                    prot.push_back(AA1::L);
+                    break;
+                case 'M':
+                case 'm':
+                    prot.push_back(AA1::M);
+                    break;
+                case 'N':
+                case 'n':
+                    prot.push_back(AA1::N);
+                    break;
+                case 'P':
+                case 'p':
+                    prot.push_back(AA1::P);
+                    break;
+                case 'Q':
+                case 'q':
+                    prot.push_back(AA1::Q);
+                    break;
+                case 'R':
+                case 'r':
+                    prot.push_back(AA1::R);
+                    break;
+                case 'S':
+                case 's':
+                    prot.push_back(AA1::S);
+                    break;
+                case 'T':
+                case 't':
+                    prot.push_back(AA1::T);
+                    break;
+                case 'V':
+                case 'v':
+                    prot.push_back(AA1::V);
+                    break;
+                case 'W':
+                case 'w':
+                    prot.push_back(AA1::W);
+                    break;
+                case 'X':
+                case 'x':
+                    prot.push_back(AA1::X);
+                    break;
+                case 'Y':
+                case 'y':
+                    prot.push_back(AA1::Y);
+                    break;
+            }
+        }
+    }
+    std::string toString1() const {
+        std::string str = "";
+        for (const AA1& x: prot) {
+            str += AA1toChar(x);
+        }
+        return str;
+    }
+    
+    std::string toString3() const {
+        std::string str = "";
+        for (const AA1& x: prot) {
+            str += AA1toStr(x)+' ';
+        }
+        return str;
+    }
+    
+    Protein& operator+=(const Protein& p) {
+        for (AA1 x: p.prot) {
+            prot.push_back(x);
+        }
+        return *this;
+    }
+    
+    Protein& operator+(const Protein& p) {
+        return *this += p;
+    }
+    
+    friend std::ostream& operator<<(std::ostream& os, const Protein& p) {
+        os << p.toString1();
+        return os;
+    }
+};
 
 
 class DNA{
