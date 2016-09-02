dfb02d8c29eacd7471d3f12bb46cd665cfd2d33e
Ruqiang Liang <ruqiang.liang@gmail.com>
2016-09-02 12:36:59 -0700

    add optionals

--- getScoreFromPDB.py
+++ getScoreFromPDB.py
@@ -1,6 +1,6 @@
 import os
 import pandas as pd
-
+import argparse
 
 def myValues(f):
     myf = open(f,'r')
@@ -8,7 +8,7 @@ def myValues(f):
     myf.close()
     (st,ed)=(0,0)
     res=[]
-    res.append(f[:-4])
+    res.append("2_"+ os.path.basename(f)[:-4])
     for i in range(len(lns)):
         if lns[i].find('ClassicGrid_grid_X') == 0:
             st = i
@@ -38,11 +38,22 @@ def getNames(f):
         res.append(str(wds[0]))
     return res
 
-if __name__ = '__main__':
-    fl=[f for f in os.listdir('.') if f.endswith('.pdb')]
-    lab = getNames(fl[0])
-    test=[]
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='get scores from PDB file from Rosetta')
+    parser.add_argument('-d', action='store', help='dir to be walked', required=True)
+    parser.add_argument('-o', action='store', help='output file name', required=True)
+    args = parser.parse_args()
+    abspath = os.path.abspath(args.d)
+    outf = os.path.abspath(args.o)
+    
+    fl=[f for f in os.listdir(abspath) if f.endswith('.pdb')]
+    myp=[]
     for f in fl:
+        myp.append(os.path.join(abspath,f))
+        
+    lab = getNames(myp[0])
+    test=[]
+    for f in myp:
         test.append(myValues(f))
     df = pd.DataFrame(test, columns = lab)
-    df.to_csv('design_results.sc',index=False)
+    df.to_csv(outf, index=False, sep='\t' )
