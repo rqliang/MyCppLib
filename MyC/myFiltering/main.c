c905c51f0d9df7db0001df5586d0737d4cafbe97
Ruqiang Liang <rliang@MBIO-9421.hs.ucdhs.ucdavis.edu>
2016-09-07 15:08:35 -0700

    split long lines to make it readable.

--- MyC/myFiltering/main.c
+++ MyC/myFiltering/main.c
@@ -31,10 +31,36 @@ int main(int argc, const char * argv[]) {
     if(fp = fopen(argv[1],"r")) {
         fgets(line, 2048, fp);
         fgets(line, 2048, fp);
-        sscanf(line, "%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s", &disposible,labels[0], labels[1],labels[2],labels[3],labels[4], labels[5],labels[6],labels[7],labels[8], labels[9],labels[10],labels[11],labels[12], labels[13],labels[14],labels[15],labels[16], labels[17],labels[18],labels[19],labels[20], labels[21],labels[22],labels[23],labels[24], labels[25],labels[26],labels[27],labels[28], labels[29],labels[30],labels[31],labels[32],labels[33], labels[34],labels[35],labels[36],labels[37], labels[38],labels[39],labels[40],labels[41], labels[42],labels[43],labels[44],labels[45], labels[46],labels[47],labels[48],labels[49], labels[50],labels[51],labels[52],labels[53], labels[54],labels[55],labels[56]);
+        sscanf(line, "%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s"
+               "%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s%s", \
+               &disposible,labels[0], labels[1],labels[2],labels[3],labels[4], labels[5], \
+               labels[6],labels[7],labels[8], labels[9],labels[10],labels[11],labels[12], \
+               labels[13],labels[14],labels[15],labels[16], labels[17],labels[18],labels[19], \
+               labels[20], labels[21],labels[22],labels[23],labels[24], labels[25],labels[26], \
+               labels[27],labels[28], labels[29],labels[30],labels[31],labels[32],labels[33], \
+               labels[34],labels[35],labels[36],labels[37], labels[38],labels[39],labels[40], \
+               labels[41], labels[42],labels[43],labels[44],labels[45], labels[46],labels[47], \
+               labels[48],labels[49], labels[50],labels[51],labels[52],labels[53], labels[54], \
+               labels[55],labels[56]);
         
         for (i=0;fgets(line, 2048, fp); ++i)
-            sscanf(line, "%s%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%s", &disposible,&rec[i].scores[0], &rec[i].scores[1],&rec[i].scores[2],&rec[i].scores[3],&rec[i].scores[4], &rec[i].scores[5],&rec[i].scores[6],&rec[i].scores[7],&rec[i].scores[8], &rec[i].scores[9],&rec[i].scores[10],&rec[i].scores[11],&rec[i].scores[12], &rec[i].scores[13],&rec[i].scores[14],&rec[i].scores[15],&rec[i].scores[16], &rec[i].scores[17],&rec[i].scores[18],&rec[i].scores[19],&rec[i].scores[20], &rec[i].scores[21],&rec[i].scores[22],&rec[i].scores[23],&rec[i].scores[24], &rec[i].scores[25],&rec[i].scores[26],&rec[i].scores[27],&rec[i].scores[28], &rec[i].scores[29],&rec[i].scores[30],&rec[i].scores[31],&rec[i].scores[32],&rec[i].scores[33], &rec[i].scores[34],&rec[i].scores[35],&rec[i].scores[36],&rec[i].scores[37], &rec[i].scores[38],&rec[i].scores[39],&rec[i].scores[40],&rec[i].scores[41], &rec[i].scores[42],&rec[i].scores[43],&rec[i].scores[44],&rec[i].scores[45], &rec[i].scores[46],&rec[i].scores[47],&rec[i].scores[48],&rec[i].scores[49], &rec[i].scores[50],&rec[i].scores[51],&rec[i].scores[52],&rec[i].scores[53], &rec[i].scores[54],&rec[i].scores[55], &rec[i].name);
+            sscanf(line, "%s%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f"
+                   "%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%f%s", \
+                   &disposible,&rec[i].scores[0], &rec[i].scores[1],&rec[i].scores[2], \
+                   &rec[i].scores[3],&rec[i].scores[4], &rec[i].scores[5],&rec[i].scores[6], \
+                   &rec[i].scores[7],&rec[i].scores[8], &rec[i].scores[9],&rec[i].scores[10], \
+                   &rec[i].scores[11],&rec[i].scores[12], &rec[i].scores[13],&rec[i].scores[14], \
+                   &rec[i].scores[15],&rec[i].scores[16], &rec[i].scores[17],&rec[i].scores[18], \
+                   &rec[i].scores[19],&rec[i].scores[20], &rec[i].scores[21],&rec[i].scores[22], \
+                   &rec[i].scores[23],&rec[i].scores[24], &rec[i].scores[25],&rec[i].scores[26], \
+                   &rec[i].scores[27],&rec[i].scores[28], &rec[i].scores[29],&rec[i].scores[30], \
+                   &rec[i].scores[31],&rec[i].scores[32],&rec[i].scores[33], &rec[i].scores[34], \
+                   &rec[i].scores[35],&rec[i].scores[36],&rec[i].scores[37], &rec[i].scores[38], \
+                   &rec[i].scores[39],&rec[i].scores[40],&rec[i].scores[41], &rec[i].scores[42], \
+                   &rec[i].scores[43],&rec[i].scores[44],&rec[i].scores[45], &rec[i].scores[46], \
+                   &rec[i].scores[47],&rec[i].scores[48],&rec[i].scores[49], &rec[i].scores[50], \
+                   &rec[i].scores[51],&rec[i].scores[52],&rec[i].scores[53], &rec[i].scores[54], \
+                   &rec[i].scores[55], &rec[i].name);
             
     }
     fclose(fp);
